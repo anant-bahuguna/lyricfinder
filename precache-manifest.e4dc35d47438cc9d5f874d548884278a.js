702c7561e9b8300711aa9431c9a2ec7979dd227d
anant-bahuguna <anantbahugunaddn@gmail.com>
2020-12-02 12:12:50 +0530

    Updates

---
+++ precache-manifest.e4dc35d47438cc9d5f874d548884278a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0893870a8d0e155145c57a594c0f21e8",
+    "url": "/lyricfinder/index.html"
+  },
+  {
+    "revision": "26dede2b2e0ff1ab642d",
+    "url": "/lyricfinder/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "ac6ba3e676f69491a916",
+    "url": "/lyricfinder/static/js/2.0462ecda.chunk.js"
+  },
+  {
+    "revision": "26dede2b2e0ff1ab642d",
+    "url": "/lyricfinder/static/js/main.5c121ab4.chunk.js"
+  },
+  {
+    "revision": "3739494e48b3bf7a28fe",
+    "url": "/lyricfinder/static/js/runtime~main.0f33ba3e.js"
+  }
+]);
